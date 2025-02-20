1a049726311991d3074714ffde27e5fe1b13f45f
念 <3209752489@qq.com>
2025-02-20 13:03:41 +0800

    feat(models, helpers, common, interface, js, parser, task): 添加 cookieJar 属性和相关功能 新增 cookieJar 属性，完善相关模型、接口及任务处理逻辑。

--- entry/src/main/ets/common/Type.ts
+++ entry/src/main/ets/common/Type.ts
@@ -4,10 +4,20 @@ export type Callback2<T, K = T, V = void> = (param1: T, param2: K) => V;
 
 export type Callback3<T, K = T, H = T, V = void> = (param1: T, param2: K, param3: H) => V;
 
+export type CallbackParams<T = Object, V = void> = (...params: T[]) => V;
+
 export type AsyncCallback<T, V = void> = (data: T) => Promise<V>;
 
 export type AsyncCallback2<T, K = T, V = void> = (param1: T, param2: K) => Promise<V>;
 
 export type AsyncCallback3<T, K = T, H = T, V = void> = (param1: T, param2: K, param3: H) => Promise<V>;
 
-export type TCustomBuilder = (...params: Object[]) => void;
+export type AsyncCallbackParams<T = Object, V = Promise<void>> = (...params: T[]) => V;
+
+export type TCustomBuilder = CallbackParams;
+
+export type TParserCallback<T> = AsyncCallback2<string, Record<string, Object>, T>;
+
+export type TAny = any;
+
+export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'HEAD' | 'OPTIONS' | 'CONNECT' | 'TRACE';
--- entry/src/main/resources/rawfile/js.js
+++ entry/src/main/resources/rawfile/js.js
@@ -1,9 +1,90 @@
+/**
+ * ETS 中的函数
+ */
 class EtsBridge {
-  exeEval(str) {
-    return eval(str)
+  /**
+   * ets.info
+   * @param {string[]} args
+   */
+  info(...args) {
+    dsBridge.call('ets.info', args)
+  }
+
+  /**
+   * ets.warn
+   * @param {string[]} args
+   */
+  warn(...args) {
+    dsBridge.call('ets.warn', args)
+  }
+
+  /**
+   * ets.warn
+   * @param {string[]} args
+   */
+  error(...args) {
+    dsBridge.call('ets.error', args)
+  }
+}
+
+/**
+ * 获取错误对象
+ * @param {Error} e
+ * @returns
+ */
+function _getError(e) {
+  return { message: e.message, stack: e.stack, error: true }
+}
+
+/**
+ * 注册可以在 ETS 中调用的函数, 同步函数
+ */
+class JsBridge {
+
+}
+
+/**
+ * 注册可以在 ETS 中调用的函数, 异步函数
+ */
+class JsBridgeAsync {
+  /**
+   * 执行 eval 函数
+   * @param {string} code 代码
+   * @param {(...arg) => void} callback 结果回调
+   */
+  async exeEval(code, callback) {
+    try {
+      let value = eval(code);
+      if (value instanceof Promise) {
+        value = await value;
+      }
+      callback(value)
+    } catch (e) {
+      callback(_getError(e))
+    }
+  }
+
+  /**
+   * 添加库
+   * @param {string[]} libs 脚本库集合
+   * @param {(...arg) => void} callback 结果回调
+   * @see https://www.cnblogs.com/kunmomo/p/12153492.html
+   */
+  async addLibs(libs, callback) {
+    try {
+      $LAB.setOptions({ AlwaysPreserveOrder: true })
+        .script(...libs)
+        .wait(() => callback(true))
+    } catch (e) {
+      callback(_getError(e))
+    }
   }
 }
 
 
-// 注册同步函数
-dsBridge.register('js', new EtsBridge())
+// 注册可以在 ETS 中调用的函数, 同步函数
+dsBridge.register('js', new JsBridge())
+// 注册可以在 ETS 中调用的函数, 异步函数
+dsBridge.registerAsyn('js', new JsBridgeAsync())
+// 注册 ETS 中调用的函数
+window.ets = new EtsBridge()
