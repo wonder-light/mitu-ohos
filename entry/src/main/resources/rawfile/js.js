aa2e272a7cfb5729d14bd6ceba2db454437da9c6
念 <3209752489@qq.com>
2025-02-21 12:05:48 +0800

    feat: 添加 jsonpath-plus.min.js 支持并实现相关查询功能

--- entry/src/main/resources/rawfile/js.js
+++ entry/src/main/resources/rawfile/js.js
@@ -37,6 +37,16 @@ function _getError(e) {
 }
 
 /**
+ * 将字符串解析为 DOM 对象
+ * @param {string} html
+ * @returns {Document}
+ */
+function domParse(html) {
+  let parser = new DOMParser();
+  return parser.parseFromString(html, 'application/xhtml+xml')
+}
+
+/**
  * 注册可以在 ETS 中调用的函数, 同步函数
  */
 class JsBridge {
@@ -76,6 +86,65 @@ class JsBridgeAsync {
         .script(...libs)
         .wait(() => callback(true))
     } catch (e) {
+      callback(_getError(e))
+    }
+  }
+
+  /**
+   * 执行 JsonPath 查询
+   * @param json json 字符串
+   * @param path JsonPath 字符串
+   * @param callback
+   */
+  async jsonPath(json, path, callback) {
+    try {
+      let value = JSONPath({ path, json });
+      callback(value)
+    } catch (e) {
+      callback(_getError(e))
+    }
+  }
+
+  /**
+   * 执行 xpath 查询
+   * @param html html 字符串
+   * @param path xpath 字符串
+   * @param isHtml 是否需要返回 html 字符串
+   * @param callback
+   */
+  async htmlPath(html, path, isHtml, callback) {
+    let dom;
+    try {
+      dom = domParse(html);
+    } catch (e) {
+      callback(_getError(e))
+    }
+    let error = null;
+    // xpath
+    try {
+      let values = [];
+      let nodes = dom.evaluate(path, dom, null, XPathResult.ANY_TYPE, null);
+      let node = null;
+      while ((node = nodes.iterateNext()) != null) {
+        values.push(isHtml == true ? (node.outerHTML ?? node.textContent) : node.textContent)
+      }
+      callback(values);
+      return;
+    } catch (e) {
+      error = e
+    }
+    // css query
+    try {
+      let values = [];
+      let nodes = dom.querySelectorAll(path);
+      for (let i = 0; i < nodes.length; i++) {
+        let node = nodes[i];
+        values.push(isHtml == true ? (node.outerHTML ?? node.textContent) : node.textContent)
+      }
+      callback(values);
+    } catch (e) {
+      e.message = error.message + '\n\n' + e.message;
+      e.stack = error.stack + '\n\n' + e.stack;
       callback(_getError(e))
     }
   }
